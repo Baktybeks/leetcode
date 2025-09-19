938a3781484cf716d12cbcb94cd1dd98942fcdfd
Baktybeks <baktybeks@gmail.com>
2025-09-19 14:35:43 +0600

    0035

---
+++ easy/0035.SearchInsertPositionю.js
@@ -0,0 +1,11 @@
+function searchInsert(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+    return nums.length;
+}
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6, 9], 10));

5e6f6fd41b82327d1c65cac797d6de589042e6ae
Baktybeks <baktybeks@gmail.com>
2025-09-21 13:13:24 +0600

    69

---
+++ easy/0069.Sqrt(x).ts
@@ -0,0 +1,29 @@
+function mySqrt(x: number): number {
+  return Math.floor(Math.sqrt(x));
+}
+
+console.log(mySqrt(4));
+
+// function mySqrt(x: number): number {
+//   if (x < 2) return x;
+
+//   let left = 2;
+//   let right = Math.floor(x / 2);
+//   let ans = 0;
+
+//   while (left <= right) {
+//     const mid = Math.floor((left + right) / 2);
+//     const squared = mid * mid;
+
+//     if (squared === x) {
+//       return mid;
+//     } else if (squared < x) {
+//       ans = mid;
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+
+//   return ans;
+// }

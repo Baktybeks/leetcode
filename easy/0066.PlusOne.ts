a01c67f9e407fecd9275863e169ea69e7fe2d07d
Baktybeks <baktybeks@mail.ru>
2025-10-02 23:23:24 +0600

    0058

---
+++ easy/0066.PlusOne.ts
@@ -0,0 +1,13 @@
+function plusOne(digits: number[]): number[] {
+  const n: number = digits.length;
+
+  for (let i = n - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+
+  return [1, ...digits];
+}

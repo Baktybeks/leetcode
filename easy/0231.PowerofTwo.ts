e2290b9550081172ff5297f37f2e06d4755b5548
Baktybeks <baktybeks@gmail.com>
2025-10-13 22:19:25 +0600

    easy/0231.PowerofTwo.ts

---
+++ easy/0231.PowerofTwo.ts
@@ -0,0 +1,20 @@
+function isPowerOfTwo(n: number): boolean {
+  if (n <= 0) return false;
+  return Math.log2(n) % 1 === 0;
+}
+
+console.log(isPowerOfTwo(16));
+
+// function isPowerOfTwo(n: number): boolean {
+//     if (n==1) return true;
+//     // return (n/2)&2 != 0  ? false : true;
+//     while(n > 0){
+//         // n = n/2
+//         if(n==1){
+//             return true;
+//         }
+//         n = n/2
+//     }
+//     return false
+
+// };
